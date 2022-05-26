4e1a8f65f5a9966a694d940ee8b3ac3a8a586fe1
KYEONGMIN-LEE <106309553+KYEONGMIN-LEE@users.noreply.github.com>
2022-05-26 20:21:55 +0900

    Update homework.sh

--- homework.sh
+++ homework.sh
@@ -11,3 +11,12 @@ git init
 git config --global user.name "KYEOM LEE'
 git config --global user.email kibo2079@knou.ac.kr
 git config --global color.ui false
+
+git branch com_1
+
+git merge com_1
+
+git remote add origin https://github.com/KYEONGMIN-LEE/DEVELOPSW.git
+
+git push -u origin main
+
