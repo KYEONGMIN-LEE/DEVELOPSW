76bf9afa0dbfa48be6b4632281f139cbbd6f9036
KYEONGMIN-LEE <106309553+KYEONGMIN-LEE@users.noreply.github.com>
2022-05-26 20:02:20 +0900

    Update homework.sh

--- homework.sh
+++ homework.sh
@@ -4,3 +4,10 @@ git commit -m "work 1"
 echo commit2 > homework.txt
 git add homework.txt
 git commit -m "work 2"
+
+mkdir git_project
+cd git_project
+git init
+git config --global user.name "KYEOM LEE'
+git config --global user.email kibo2079@knou.ac.kr
+git config --global color.ui false
